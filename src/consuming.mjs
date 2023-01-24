b2a7db69af23777e6a808def92eaa33995c47d94
Malhar Bhuptani <mbhuptani@ipdanalytics.com>
2023-01-24 15:51:51 +0530

    consuming promises

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,10 +1,17 @@
 import setText, { appendText, showWaiting, hideWaiting } from "./results.mjs";
 
 export function get() {
-  axios.get("http://localhost:3000/orders/1");
+  axios.get("http://localhost:3000/orders/1").then(({ data }) => {
+    setText(JSON.stringify(data));
+  });
 }
 
-export function getCatch() {}
+export function getCatch() {
+  axios
+    .get("http://localhost:3000/orders/123")
+    .then(({ data }) => setText(JSON.stringify(data)))
+    .catch((err) => setText(err));
+}
 
 export function chain() {}
 
